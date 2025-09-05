cbc1f8d16df2cdce946d68129b282647e7b85abe
Bianca <lopez.343955@balayan.sti.edu.ph>
2025-09-06 00:08:53 +0800

    Added form for Login Page

---
+++ EventDriven.Project.Enrollment.UI/StartPage.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace EventDriven.Project.Enrollment.UI
+{
+    partial class StartPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StartPage));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblBNHS = new System.Windows.Forms.Label();
+            this.ptbLogo1 = new SATAUiFramework.Controls.SATAPictureBox();
+            this.ptgBG = new System.Windows.Forms.PictureBox();
+            this.lblEnrollManageSys = new System.Windows.Forms.Label();
+            this.ptbLogo2 = new SATAUiFramework.Controls.SATAPictureBox();
+            this.btnContinue = new FrameworkTest.SATAButton();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbLogo1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptgBG)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbLogo2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+            this.panel1.Controls.Add(this.lblBNHS);
+            this.panel1.Controls.Add(this.ptbLogo1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(957, 68);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblBNHS
+            // 
+            this.lblBNHS.AutoSize = true;
+            this.lblBNHS.Font = new System.Drawing.Font("Britannic Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBNHS.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblBNHS.Location = new System.Drawing.Point(121, 20);
+            this.lblBNHS.Name = "lblBNHS";
+            this.lblBNHS.Size = new System.Drawing.Size(307, 26);
+            this.lblBNHS.TabIndex = 2;
+            this.lblBNHS.Text = "Balayan National High School";
+            // 
+            // ptbLogo1
+            // 
+            this.ptbLogo1.BorderCapStyle = System.Drawing.Drawing2D.DashCap.Flat;
+            this.ptbLogo1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+            this.ptbLogo1.BorderColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+            this.ptbLogo1.BorderLineStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+            this.ptbLogo1.BorderSize = 1;
+            this.ptbLogo1.GradientAngle = 50F;
+            this.ptbLogo1.Image = ((System.Drawing.Image)(resources.GetObject("ptbLogo1.Image")));
+            this.ptbLogo1.Location = new System.Drawing.Point(27, -16);
+            this.ptbLogo1.Name = "ptbLogo1";
+            this.ptbLogo1.Size = new System.Drawing.Size(96, 96);
+            this.ptbLogo1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ptbLogo1.TabIndex = 1;
+            this.ptbLogo1.TabStop = false;
+            // 
+            // ptgBG
+            // 
+            this.ptgBG.Image = ((System.Drawing.Image)(resources.GetObject("ptgBG.Image")));
+            this.ptgBG.Location = new System.Drawing.Point(-55, 63);
+            this.ptgBG.Name = "ptgBG";
+            this.ptgBG.Size = new System.Drawing.Size(1068, 540);
+            this.ptgBG.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ptgBG.TabIndex = 1;
+            this.ptgBG.TabStop = false;
+            // 
+            // lblEnrollManageSys
+            // 
+            this.lblEnrollManageSys.AutoSize = true;
+            this.lblEnrollManageSys.BackColor = System.Drawing.Color.Transparent;
+            this.lblEnrollManageSys.Font = new System.Drawing.Font("Britannic Bold", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnrollManageSys.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblEnrollManageSys.Location = new System.Drawing.Point(195, 113);
+            this.lblEnrollManageSys.Name = "lblEnrollManageSys";
+            this.lblEnrollManageSys.Size = new System.Drawing.Size(540, 41);
+            this.lblEnrollManageSys.TabIndex = 3;
+            this.lblEnrollManageSys.Text = "Enrollment Management System";
+            // 
+            // ptbLogo2
+            // 
+            this.ptbLogo2.BackColor = System.Drawing.Color.Transparent;
+            this.ptbLogo2.BorderCapStyle = System.Drawing.Drawing2D.DashCap.Flat;
+            this.ptbLogo2.BorderColor = System.Drawing.Color.Transparent;
+            this.ptbLogo2.BorderColor2 = System.Drawing.Color.Transparent;
+            this.ptbLogo2.BorderLineStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+            this.ptbLogo2.BorderSize = 1;
+            this.ptbLogo2.GradientAngle = 50F;
+            this.ptbLogo2.Image = ((System.Drawing.Image)(resources.GetObject("ptbLogo2.Image")));
+            this.ptbLogo2.Location = new System.Drawing.Point(319, 166);
+            this.ptbLogo2.Name = "ptbLogo2";
+            this.ptbLogo2.Size = new System.Drawing.Size(284, 284);
+            this.ptbLogo2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ptbLogo2.TabIndex = 3;
+            this.ptbLogo2.TabStop = false;
+            // 
+            // btnContinue
+            // 
+            this.btnContinue.ButtonText = "Continue";
+            this.btnContinue.CheckedBackground = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+            this.btnContinue.CheckedForeColor = System.Drawing.Color.White;
+            this.btnContinue.CheckedImageTint = System.Drawing.Color.White;
+            this.btnContinue.CheckedOutline = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+            this.btnContinue.CustomDialogResult = System.Windows.Forms.DialogResult.None;
+            this.btnContinue.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnContinue.HoverBackground = System.Drawing.Color.IndianRed;
+            this.btnContinue.HoverForeColor = System.Drawing.SystemColors.Window;
+            this.btnContinue.HoverImage = null;
+            this.btnContinue.HoverImageTint = System.Drawing.Color.White;
+            this.btnContinue.HoverOutline = System.Drawing.Color.Empty;
+            this.btnContinue.Image = null;
+            this.btnContinue.ImageAutoCenter = true;
+            this.btnContinue.ImageExpand = new System.Drawing.Point(0, 0);
+            this.btnContinue.ImageOffset = new System.Drawing.Point(0, 0);
+            this.btnContinue.ImageTint = System.Drawing.Color.White;
+            this.btnContinue.IsToggleButton = false;
+            this.btnContinue.IsToggled = false;
+            this.btnContinue.Location = new System.Drawing.Point(354, 482);
+            this.btnContinue.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnContinue.Name = "btnContinue";
+            this.btnContinue.NormalBackground = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+            this.btnContinue.NormalForeColor = System.Drawing.Color.White;
+            this.btnContinue.NormalOutline = System.Drawing.Color.Empty;
+            this.btnContinue.OutlineThickness = 2F;
+            this.btnContinue.PressedBackground = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+            this.btnContinue.PressedForeColor = System.Drawing.Color.White;
+            this.btnContinue.PressedImageTint = System.Drawing.Color.White;
+            this.btnContinue.PressedOutline = System.Drawing.Color.Empty;
+            this.btnContinue.Rounding = new System.Windows.Forms.Padding(5);
+            this.btnContinue.Size = new System.Drawing.Size(217, 46);
+            this.btnContinue.TabIndex = 0;
+            this.btnContinue.TextAutoCenter = true;
+            this.btnContinue.TextOffset = new System.Drawing.Point(0, 0);
+            // 
+            // StartPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(957, 577);
+            this.Controls.Add(this.btnContinue);
+            this.Controls.Add(this.ptbLogo2);
+            this.Controls.Add(this.lblEnrollManageSys);
+            this.Controls.Add(this.ptgBG);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "StartPage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbLogo1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptgBG)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbLogo2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private SATAUiFramework.Controls.SATAPictureBox ptbLogo1;
+        private System.Windows.Forms.PictureBox ptgBG;
+        private System.Windows.Forms.Label lblBNHS;
+        private System.Windows.Forms.Label lblEnrollManageSys;
+        private SATAUiFramework.Controls.SATAPictureBox ptbLogo2;
+        private FrameworkTest.SATAButton btnContinue;
+    }
+}
+
